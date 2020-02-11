3eef008eac7570db9de6cde1e5cadd5c2b5022e9
Dammir2805 <d_kalymbaev@mail.ru>
2020-02-11 15:36:19 +0300

    Currancy rate calculator

---
+++ currcalc.rb
@@ -0,0 +1,24 @@
+#Hi guys, this program is for calculating currant currency rate
+#Datas are gotten from http://www.cbr.ru/scripts/XML_daily.asp
+
+#Turning on dependencies
+
+require "net/http"
+require "uri"
+require "rexml/document"
+
+#where uri request is sent
+uri = URI.parse("http://www.cbr.ru/scripts/XML_daily.asp")
+
+#getting response
+response = Net::HTTP.get_response(uri)
+
+#parsing response
+doc = REXML::Document.new(response.body)
+
+currant_rate = doc.root.elements.to_a[8]
+
+hkd = currant_rate.elements['Name']
+rate = currant_rate.elements['Value']
+
+puts "Курс ЦБ России 10 #{hkd} равен #{rate} рублей"
